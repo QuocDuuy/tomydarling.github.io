ac13605268c7553cb942c5bcc5463ee7c2d8c993
Phạm Trần Quốc Duy <81229206+QuocDuuy@users.noreply.github.com>
2023-06-20 22:39:36 +0700

    Delete PresentForDarling directory

--- PresentForDarling/config.js
+++
@@ -1,9 +0,0 @@
-const nameGirl = 'Nhã Hân';
-const giftUrl = 'http://nodemy.vn';
-const eventName = 'Cảm ơn em';
-const titleCard = 'Tặng người anh yêu';
-const contentCard = 'Cám ơn em - Người con gái mà anh thương yêu nhất suốt thời gian qua đã luôn quan tâm và chăm sóc cho anh. Cám ơn em vì đã yêu anh. Cám ơn em vì đã đến với cuộc đời của anh, vực anh dậy và cho anh ngày hôm nay. ANH YÊU EM';
-
-const giftImage = 'emYeu.JPG';
-const base64 = '';
-const giftImageBase64 = "data:image/png;base64, " + base64;
