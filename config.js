894cf53c70951716c7ec6fd92ca83b4a1520d641
Duy <p.t.q.duy192002@gmail.com>
2023-09-01 22:04:23 +0700

    initial commit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'Nhã Hân';
-// const giftUrl = 'http://nodemy.vn';
+const giftUrl = '';
 const eventName = 'Cảm ơn em';
 const titleCard = 'Tặng người anh yêu';
 const contentCard = 'Cám ơn em - Người con gái mà anh thương yêu nhất suốt thời gian qua đã luôn quan tâm và chăm sóc cho anh. Cám ơn em vì đã yêu anh. Cám ơn em vì đã đến với cuộc đời của anh, vực anh dậy và cho anh ngày hôm nay. ANH YÊU EM';
